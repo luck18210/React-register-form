45640ef0ac30866c54ff21591c7687bb0542c8ac
lucky18210 <webdev0084@gmail.com>
2022-06-22 00:49:22 -0500

    first commit React-register-form

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Register = React.lazy(() => import("./pages/auth/register"));
+
+const App = () => {
+  return (
+    <>
+      <Suspense fallback={<></>}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Register />}></Route>
+          </Routes>
+        </Router>
+      </Suspense>
+    </>
+  );
+};
+
+export default App;
